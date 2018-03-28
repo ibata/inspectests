25fa365581edf4878b4cb319514d0e79480cf83d
ibata <luc.ibata@yahoo.com>
2018-03-28 08:13:11 -0700

    Update reach_host.rb

--- controls/reach_host.rb
+++ controls/reach_host.rb
@@ -2,7 +2,11 @@ control 'reach_host' do
   impact 1.0
   title 'This is Testing Endpoint'
   describe command("curl -sS http://dc5-glue1-1-p2:9090/1/0/customer/customers") do
-    its('status') { should eq 200 }
     its('stderr') { should eq ''}
+  end
+  describe http("https://ec2us-admin-nurding.anaplan.com/administration/about.html", ssl_verify: false) do
+    its('status') { should eq 200 }
+    its('body') { should cmp %r{<h1>administration</h1>} }
+    its('body') { should cmp %r{<p>Anaplan Administration</p>} }
   end
 end
