40c1095309ae0ca75e2a50dfd57063e92fb5771b
ibata <luc.ibata@yahoo.com>
2018-03-28 16:40:36 -0700

    Update reach_host.rb

--- controls/reach_host.rb
+++ controls/reach_host.rb
@@ -4,11 +4,11 @@
 control 'reach_host' do
   impact 1.0
   title 'This is Testing Endpoint'
-  describe http("http://ec2us-glue1-1-demoluc.anaplan.com:8080/about", ssl_verify: false, enable_remote_worker: true) do
+  describe http("http://XXX:8080/about", ssl_verify: false, enable_remote_worker: true) do
     its('status') { should eq 200 }
     its('body') { should cmp %r{<h3>Build data:</h3>} }
   end
-  describe http("http://ec2us-glue1-1-arcus.anaplan.com:8080/about", ssl_verify: false, enable_remote_worker: true) do
+  describe http("http://XXX:8080/about", ssl_verify: false, enable_remote_worker: true) do
     its('status') { should eq 200 }
     its('body') { should cmp %r{<h3>Build data:</h3>} }
   end
