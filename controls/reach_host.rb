aa1e47745ecc2c39a297d19b060c6816c7c5f30f
ibata <luc.ibata@yahoo.com>
2018-03-28 08:08:09 -0700

    Update reach_host.rb

--- controls/reach_host.rb
+++ controls/reach_host.rb
@@ -2,7 +2,7 @@ control 'reach_host' do
   impact 1.0
   title 'This is Testing Endpoint'
   describe command("curl -sS http://dc5-glue1-1-p2:9090/1/0/customer/customers") do
-#    its('stdout') { should_not match /HTTP Status 404/i }
+    its('stdout') { should_not match /HTTP Status 404/i }
     its('stderr') { should eq ''}
   end
 end
