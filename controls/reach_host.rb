4ce2b7c4ea6618571debbb8e92f09a663bc7a5ba
ibata <luc.ibata@yahoo.com>
2018-03-28 15:11:22 -0700

    Update reach_host.rb

--- controls/reach_host.rb
+++ controls/reach_host.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+# encoding: utf-8
+
 control 'reach_host' do
   impact 1.0
   title 'This is Testing Endpoint'
-  describe command("curl -sS http://dc5-glue1-1-p2:9090/1/0/customer/customers") do
-    its('stderr') { should eq ''}
+
+  describe http("http://ec2us-glue1-1-arcus.anaplan.com:8080/about", ssl_verify: false) do
+    its('status') { should eq 200 }
+    its('body') { should cmp %r{<h3>Build data:</h3>} }
   end
 
-  describe http("https://ec2us-admin-nurding.anaplan.com/administration/about.html", ssl_verify: false) do
-    its('status') { should eq 200 }
-    its('body') { should cmp %r{<h1>administration</h1>} }
-    its('body') { should cmp %r{<p>Anaplan Administration</p>} }
-  end
 end
+# #fqdn = sys_info.hostname
+
