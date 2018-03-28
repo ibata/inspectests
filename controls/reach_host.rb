1795e1d26c9c8eab26295e4d629a77babf770333
ibata <luc.ibata@yahoo.com>
2018-03-28 08:14:48 -0700

    Update reach_host.rb

--- controls/reach_host.rb
+++ controls/reach_host.rb
@@ -4,6 +4,11 @@ control 'reach_host' do
   describe command("curl -sS http://dc5-glue1-1-p2:9090/1/0/customer/customers") do
     its('stderr') { should eq ''}
   end
+  describe http("https://dc5-glue1-1-p2:9090/1/0/customer/customers", ssl_verify: false) do
+    its('status') { should eq 200 }
+    its('body') { should cmp %r{<h1>administration</h1>} }
+    its('body') { should cmp %r{<p>Anaplan Administration</p>} }
+  end
   describe http("https://ec2us-admin-nurding.anaplan.com/administration/about.html", ssl_verify: false) do
     its('status') { should eq 200 }
     its('body') { should cmp %r{<h1>administration</h1>} }
